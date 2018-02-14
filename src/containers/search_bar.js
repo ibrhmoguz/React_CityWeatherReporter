94768732dd88fd58dac1f27a74f997df0b540102
Ibrahim OGUZ <ibrahimoguz.ie@gmail.com>
2018-02-14 12:45:06 +0200

    Search bar added.

---
+++ src/containers/search_bar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { term: '' };
+        
+        //Bind component instance to local function onInputChange.
+        this.onInputChange = this.onInputChange.bind(this); 
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+        this.setState({ term: event.target.value });
+    }
+
+    render() {
+        return (
+            <form className="input-group">
+                <input
+                    placeholder="Get a five-day forecast in your favorite cities."
+                    className="form-control"
+                    value={this.state.value}
+                    onChange={this.onInputChange} />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
+            </form>
+        );
+    }
+}
+
+export default SearchBar;
